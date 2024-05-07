4729afb1d265bff384262b988875877dbc7a8ddc
YUKI <78786632+ykshio@users.noreply.github.com>
2024-05-07 18:12:03 +0900

    update

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const input = document.getElementById('input');
-const preview = document.getElementById('preview');
+const input = document.getElementById("input");
+const preview = document.getElementById("preview");
 
-input.addEventListener('input', function() {
+input.addEventListener("input", function () {
   const text = this.value;
-  let previewText = '';
+  let previewText = "";
 
   for (let i = 0; i < text.length; i++) {
     const charCode = text.charCodeAt(i);
-    // Check if the character is a 7-bit character (ASCII characters)
-    if (charCode <= 127) {
+    if (text[i] === "\n") {
+      // Check for newline character
+      previewText += "<br>"; // Convert newline to <br> tag
+    } else if (charCode <= 127) {
       previewText += text[i];
     } else {
-      // If not a 7-bit character, apply error class to change color
       previewText += `<span class="error">${text[i]}</span>`;
     }
   }
